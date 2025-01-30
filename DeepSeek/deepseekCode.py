245d4ca89b3ecf3e15542082272a286532922731
Vinay Aadatiya <adatiyavinayshaileshbhai@gmail.com>
2025-01-30 11:35:49 +0530

    Updated code of deepseek prompt1

--- DeepSeek/deepseekCode.py
+++ DeepSeek/deepseekCode.py
@@ -1,94 +1,110 @@
 # Prompt :- Simulate a double pendulum system with chaotic motion and allow user interaction to change initial conditions. Use Python to create a real-time visualization of the system's evolution.
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 from scipy.integrate import solve_ivp
 
-def double_pendulum(t, y, L1, L2, m1, m2, g):
-    theta1, z1, theta2, z2 = y
-    
-    delta = theta2 - theta1
-    den1 = (m1 + m2) * L1 - m2 * L1 * np.cos(delta) * np.cos(delta)
-    den2 = (L2 / L1) * den1
-    
-    dtheta1 = z1
-    dz1 = (m2 * L1 * z1 * z1 * np.sin(delta) * np.cos(delta)
-           + m2 * g * np.sin(theta2) * np.cos(delta)
-           + m2 * L2 * z2 * z2 * np.sin(delta)
-           - (m1 + m2) * g * np.sin(theta1)) / den1
-    
-    dtheta2 = z2
-    dz2 = (-m2 * L2 * z2 * z2 * np.sin(delta) * np.cos(delta)
-           + (m1 + m2) * g * np.sin(theta1) * np.cos(delta)
-           - (m1 + m2) * L1 * z1 * z1 * np.sin(delta)
-           - (m1 + m2) * g * np.sin(theta2)) / den2
-    
-    return [dtheta1, dz1, dtheta2, dz2]
-
-# Parameters
-L1 = 1.0
-L2 = 1.0
-m1 = 1.0
-m2 = 1.0
-g = 9.81
+# Constants
+g = 9.81  # gravity (m/s^2)
+L1 = 1.0  # length of the first pendulum (m)
+L2 = 1.0  # length of the second pendulum (m)
+m1 = 1.0  # mass of the first pendulum (kg)
+m2 = 1.0  # mass of the second pendulum (kg)
 
 # Initial conditions
-theta1_0 = np.pi / 2
-theta2_0 = np.pi / 2
-z1_0 = 0.0
-z2_0 = 0.0
+theta1_0 = np.pi / 2  # initial angle of the first pendulum (rad)
+theta2_0 = np.pi / 2  # initial angle of the second pendulum (rad)
+omega1_0 = 0.0  # initial angular velocity of the first pendulum (rad/s)
+omega2_0 = 0.0  # initial angular velocity of the second pendulum (rad/s)
 
-y0 = [theta1_0, z1_0, theta2_0, z2_0]
+# Time parameters
+t_max = 30  # maximum time (s)
+dt = 0.05  # time step (s)
+t_eval = np.arange(0, t_max, dt)
 
-# Set up the figure and axis
-fig, ax = plt.subplots()
-plt.subplots_adjust(left=0.1, bottom=0.4)
-ax.set_xlim(-2, 2)
-ax.set_ylim(-2, 2)
+# Function to compute the derivatives of the state vector
+def derivatives(t, y):
+    theta1, theta2, omega1, omega2 = y
+    
+    # Equations of motion for a double pendulum
+    delta_theta = theta2 - theta1
+    den1 = (m1 + m2) * L1 - m2 * L1 * np.cos(delta_theta) ** 2
+    den2 = (L2 / L1) * den1
+    
+    dtheta1_dt = omega1
+    dtheta2_dt = omega2
+    
+    domega1_dt = (m2 * L2 * omega2 ** 2 * np.sin(delta_theta) -
+                  m2 * g * np.sin(theta2) * np.cos(delta_theta) +
+                  (m1 + m2) * g * np.sin(theta1)) / den1
+    
+    domega2_dt = (-L1 / L2 * omega1 ** 2 * np.sin(delta_theta) -
+                  g * np.sin(theta2) +
+                  g * np.sin(theta1) * np.cos(delta_theta)) / den2
+    
+    return [dtheta1_dt, dtheta2_dt, domega1_dt, domega2_dt]
+
+# Function to update the plot when sliders are changed
+def update(val):
+    # Get new initial conditions from sliders
+    theta1_0 = slider_theta1.val
+    theta2_0 = slider_theta2.val
+    omega1_0 = slider_omega1.val
+    omega2_0 = slider_omega2.val
+    
+    # Solve the ODE with new initial conditions
+    sol = solve_ivp(derivatives, [0, t_max], [theta1_0, theta2_0, omega1_0, omega2_0], t_eval=t_eval)
+    
+    # Update the pendulum positions
+    x1 = L1 * np.sin(sol.y[0])
+    y1 = -L1 * np.cos(sol.y[0])
+    x2 = x1 + L2 * np.sin(sol.y[1])
+    y2 = y1 - L2 * np.cos(sol.y[1])
+    
+    # Update the plot
+    line1.set_data([0, x1[-1]], [0, y1[-1]])
+    line2.set_data([x1[-1], x2[-1]], [y1[-1], y2[-1]])
+    trajectory.set_data(x2, y2)
+    fig.canvas.draw_idle()
+
+# Solve the initial ODE
+sol = solve_ivp(derivatives, [0, t_max], [theta1_0, theta2_0, omega1_0, omega2_0], t_eval=t_eval)
+
+# Extract the positions of the pendulums
+x1 = L1 * np.sin(sol.y[0])
+y1 = -L1 * np.cos(sol.y[0])
+x2 = x1 + L2 * np.sin(sol.y[1])
+y2 = y1 - L2 * np.cos(sol.y[1])
+
+# Create the plot
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.set_xlim(-2.5, 2.5)
+ax.set_ylim(-2.5, 2.5)
 ax.set_aspect('equal')
+ax.grid()
 
-# Initialize the pendulum lines
-line, = ax.plot([], [], 'o-', lw=2)
-
-# Function to update the pendulum position
-def update_pendulum(theta1, theta2):
-    x1 = L1 * np.sin(theta1)
-    y1 = -L1 * np.cos(theta1)
-    x2 = x1 + L2 * np.sin(theta2)
-    y2 = y1 - L2 * np.cos(theta2)
-    line.set_data([0, x1, x2], [0, y1, y2])
-    return line,
-
-# Function to solve the ODE and update the plot
-def animate(t):
-    sol = solve_ivp(double_pendulum, [0, t], y0, args=(L1, L2, m1, m2, g), t_eval=[t])
-    theta1, theta2 = sol.y[0, -1], sol.y[2, -1]
-    update_pendulum(theta1, theta2)
-    return line,
-
-# Animation function
-def run_animation():
-    from matplotlib.animation import FuncAnimation
-    ani = FuncAnimation(fig, animate, frames=np.linspace(0, 10, 300), interval=50, blit=True)
-    plt.show()
+# Plot the pendulum
+line1, = ax.plot([0, x1[-1]], [0, y1[-1]], 'k-', lw=2)
+line2, = ax.plot([x1[-1], x2[-1]], [y1[-1], y2[-1]], 'k-', lw=2)
+trajectory, = ax.plot(x2, y2, 'r-', lw=1)
 
 # Create sliders for initial conditions
 axcolor = 'lightgoldenrodyellow'
-ax_theta1 = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-ax_theta2 = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+ax_theta1 = plt.axes([0.2, 0.01, 0.65, 0.03], facecolor=axcolor)
+ax_theta2 = plt.axes([0.2, 0.05, 0.65, 0.03], facecolor=axcolor)
+ax_omega1 = plt.axes([0.2, 0.09, 0.65, 0.03], facecolor=axcolor)
+ax_omega2 = plt.axes([0.2, 0.13, 0.65, 0.03], facecolor=axcolor)
 
-theta1_slider = Slider(ax_theta1, 'Theta1', 0.0, 2 * np.pi, valinit=theta1_0)
-theta2_slider = Slider(ax_theta2, 'Theta2', 0.0, 2 * np.pi, valinit=theta2_0)
+slider_theta1 = Slider(ax_theta1, 'Theta1', 0, 2 * np.pi, valinit=theta1_0)
+slider_theta2 = Slider(ax_theta2, 'Theta2', 0, 2 * np.pi, valinit=theta2_0)
+slider_omega1 = Slider(ax_omega1, 'Omega1', -10, 10, valinit=omega1_0)
+slider_omega2 = Slider(ax_omega2, 'Omega2', -10, 10, valinit=omega2_0)
 
-# Update function for sliders
-def update(val):
-    global y0
-    y0[0] = theta1_slider.val
-    y0[2] = theta2_slider.val
+# Attach the update function to the sliders
+slider_theta1.on_changed(update)
+slider_theta2.on_changed(update)
+slider_omega1.on_changed(update)
+slider_omega2.on_changed(update)
 
-theta1_slider.on_changed(update)
-theta2_slider.on_changed(update)
-
-# Run the animation
-run_animation()
+# Show the plot
+plt.show()
